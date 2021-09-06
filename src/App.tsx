8ffbf0fccd1da20cf0b7e55d651dc44296261914
Stanislas Castaybert <perso@stan1712.com>
2021-09-06 20:00:17 +0200

    ⚪ other・Commit initial (React + Rsuite)

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
+import React from 'react';
 import logo from './logo.svg';
+import { Button } from 'rsuite';
 import './App.css';
+import 'rsuite/dist/styles/rsuite-default.css';
 
 function App() {
   return (
@@ -7,16 +10,16 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
+        <Button
           className="App-link"
-          href="https://reactjs.org"
+          href="https://rsuitejs.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          React Suite
+        </Button>
       </header>
     </div>
   );
