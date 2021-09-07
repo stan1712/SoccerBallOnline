0a2bff1826419f99953d39232d6449fa040504fe
Carlier Matheo <Carlier.matheo33@gmail.com>
2021-09-07 17:31:19 +0200

    ajout de l'image en background et du logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
 	<div>
-		<h1>Home</h1>
+		<div style="background-image: url(https://cdn.discordapp.com/attachments/884355153682112513/884772207832760410/PicsArt_09-07-09.46.29.png);"> 
+		<br><br><br><br><br>
+		<v-btn outlined color="primary"></v-btn>
+
+		</div>
+			<div>
+			<h1>ça marche pas woula</h1>
+		</div>
 	</div>
+
 </template>
 
 <script>
