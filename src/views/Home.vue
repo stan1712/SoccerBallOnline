0c4a4922e70996294879623a8409bfd7ede777b9
Carlier Matheo <Carlier.matheo33@gmail.com>
2021-09-08 16:45:43 +0200

    Titre -> title + button "GoTo"

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,51 @@
 <template>
 	<div>
-		<div class=big  style=" background-size: 100%; background-image: url(https://cdn.discordapp.com/attachments/884355153682112513/884772207832760410/PicsArt_09-07-09.46.29.png);"> 
-		
-			<v-btn class="d-flex justify-center" color="primary" elevation="2" outlined >
-				oui
+		<div class="big d-flex justify-center "  style=" background-size: 100%; background-image: url(https://cdn.discordapp.com/attachments/884355153682112513/884772207832760410/PicsArt_09-07-09.46.29.png);"> 
+			<v-btn class="d-flex justify-center " fab outlined @click="$vuetify.goTo('#description')">
+				<i class="fa fa-chevron-down" aria-hidden="true"></i>
 			</v-btn>
 
 		</div>
-		<div>
-			<v-card class="pa-4">
-				<v-card-titre class="d-flex justify-center"> Description du jeu </v-card-titre>
-				<br>
+		<div id="description" class="d-flex justify-center">
+			<v-card  
+			  rounded="xl"
+          	  class="mb-4 ma-2 pa-4"
+              height="300"
+              width="700"
+              elevation="2"
+              outlined>
+				<v-card-title class="d-flex justify-center"> Description du jeu </v-card-title>
+				
 				<p class="d-flex justify-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			</v-card>
 		</div>
-		<div>
-			<v-card class="pa-4">
-				<v-card-titre class="d-flex justify-center"> Trailler </v-card-titre>
-				<br>
+		<div class="d-flex justify-center">
+			<v-card 
+			  rounded="xl"
+          	  class="mb-4 ma-2 pa-4"
+              height="300"
+              width="700"
+              elevation="2"
+              outlined>
+				<v-card-title class="d-flex justify-center"> Trailler </v-card-title>
+				
 				<p class="d-flex justify-center"></p>
 			</v-card>
 		</div>
-		<div>
-			<v-card class="pa-4">
-				<v-card-titre class="d-flex justify-center"> Crédits </v-card-titre>
-				<br>
+		<div class="d-flex justify-center">
+			<v-card  
+			  rounded="xl"
+          	  class="mb-4 ma-2 pa-4"
+              height="300"
+              width="700"
+              elevation="2"
+              outlined>
+				<v-card-title class="d-flex justify-center"> Crédits </v-card-title>
+				
 				<p class="d-flex justify-center">Projet Workshop réalisé sur 5 jours par quatre B2 de l'EPSI Bordeaux et 2 SN2.</p>
-				<v-list class="d-flex justify-center"> 
+				<v-list class="d-flex justify-center"> <br>
 					Stanislas Castaybert <br>
-					Kylian Maillard <br>
+					Kilyan Maillard <br>
 					Begüm Cetin <br>
 					Matheo Carlier <br>
 
