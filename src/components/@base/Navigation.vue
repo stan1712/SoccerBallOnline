ae07a82c414304b5c8fd55c5fe1ec404a64713a7
Stanislas Castaybert <perso@stan1712.com>
2021-09-08 17:49:33 +0200

    🌈 style・Suppression du prominent et shrink on scroll

--- src/components/@base/Navigation.vue
+++ src/components/@base/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
 	<div>
 
-		<v-app-bar app dark v-bind:color="isTransparent" prominent shrink-on-scroll>
+		<v-app-bar app dark v-bind:color="isTransparent">
 			<v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://infographart.com/ressources/logos/IFGA/LogoIFGA_1_2x_W.png" transition="scale-transition" width="55" />
 
 			<router-link tag="div" :to="{ path: '/' }" style="cursor: pointer;">
