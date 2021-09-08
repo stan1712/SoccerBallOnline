f57b92d0483c85a3db8f0d7c20b69fe7fb2db94f
Stanislas Castaybert <perso@stan1712.com>
2021-09-09 00:01:09 +0200

    🌈 style・Direction Artistique

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -15,22 +15,22 @@ export default new Vuetify({
 		dark: false,
 		themes: {
 			dark: {
-				primary: '#1976D2',
-				accent: '#FF4081',
-				secondary: '#ffe18d',
-				success: '#4CAF50',
-				info: '#2196F3',
-				warning: '#FB8C00',
-				error: '#FF5252'
+				primary: '#5c7c38',
+				accent: '#009432',
+				secondary: '#26361e',
+				success: '#A3CB38',
+				info: '#12CBC4',
+				warning: '#EE5A24',
+				error: '#ED4C67'
 			},
 			light: {
-				primary: '#1976D2',
-				accent: '#e91e63',
-				secondary: '#30b1dc',
-				success: '#4CAF50',
-				info: '#2196F3',
-				warning: '#FB8C00',
-				error: '#FF5252'
+				primary: '#5c7c38',
+				accent: '#009432',
+				secondary: '#26361e',
+				success: '#A3CB38',
+				info: '#12CBC4',
+				warning: '#EE5A24',
+				error: '#ED4C67'
 			}
 		}
 	},
