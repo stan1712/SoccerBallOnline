f4a1928b90e5af93b0b39f2d6f681d18b464fc1c
Stanislas Castaybert <perso@stan1712.com>
2021-09-08 23:35:35 +0200

    🌈 style・Les formulaires sont plus petits et centrés

--- src/views/Access.vue
+++ src/views/Access.vue
@@ -1,7 +1,7 @@
 <template>
 	<v-container>
 		<v-row>
-			<v-col cols="12" sm="6">
+			<v-col cols="12" sm="3" offset="3">
 
 				<v-card>
 					<v-card-title>
@@ -20,7 +20,7 @@
 
 			</v-col>
 
-			<v-col cols="12" sm="6">
+			<v-col cols="12" sm="3">
 
 				<v-card>
 					<v-card-title>
