76cb96b31155973998490c97943e1b0adddc4920
Kilyan <kilyan.maillard@epsi.fr>
2021-09-09 01:19:11 +0200

    Suppression des arrondies, sur les cards

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -1,67 +1,139 @@
 <template>
-	<div>
+  <div>
+    <v-row>
+      <v-card
+        class="col-8 offset-2"
+        color="white"
+        height="450"
+        outlined
+        shaped
+        elevation="2"
+      >
+        <v-row>
+          <v-col cols="12" sm="6">
+            <v-card
+              class="ma-5"
+              color="white"
+              height="400"
+              outlined
+              shaped
+              elevation="2"
+            >
+              <v-card-title> Limite de temps </v-card-title>
+              <div class="d-flex flex-column mb-6">
+                <v-btn
+                  depressed
+                  class="my-3 mx-7"
+                  color="green"
+                  elevation="2"
+                  x-large
+                >
+                  1 min</v-btn
+                >
+                <v-btn
+                  depressed
+                  class="my-3 mx-7"
+                  color="green"
+                  elevation="2"
+                  x-large
+                >
+                  2 min</v-btn
+                >
+                <v-btn
+                  depressed
+                  class="my-3 mx-7"
+                  color="green"
+                  elevation="2"
+                  x-large
+                >
+                  3 min
+                </v-btn>
+              </div>
 
-		<v-row>
-			<v-card class="col-8 offset-2" color="white" height="450" outlined rounded="xl" shaped elevation="2">
-				<v-row>
-					<v-col cols="12" sm="6">
-						<v-card class="ma-5" color="white" height="400" outlined rounded="xl" shaped elevation="2">
-							<v-card-title> Limite de temps </v-card-title>
-							<div class="d-flex flex-column mb-6">
-								<v-btn depressed class="my-3 mx-7" color="green" elevation="2" x-large>
-									1 min</v-btn>
-								<v-btn depressed class="my-3 mx-7" color="green" elevation="2" x-large>
-									2 min</v-btn>
-								<v-btn depressed class="my-3 mx-7" color="green" elevation="2" x-large>
-									3 min </v-btn>
-							</div>
+              <v-btn
+                block
+                depressed
+                elevation="2"
+                fab
+                icon
+                raised
+                rounded
+                color="green"
+              >
+                Jouer
+                <v-icon>mdi-flag-variant </v-icon>
+              </v-btn>
+            </v-card>
+          </v-col>
 
+          <v-col cols="12" sm="6">
+            <v-card
+              class="ma-5"
+              color="white"
+              height="400"
+              outlined
+              shaped
+              elevation="2"
+            >
+              <v-card-title> Limite de but </v-card-title>
 
-							<v-btn block depressed elevation="2" fab icon raised rounded color="green">
-								Jouer
-								<v-icon>mdi-flag-variant
-								</v-icon>
-							</v-btn>
-						</v-card>
-					</v-col>
+              <div class="d-flex flex-column mb-6">
+                <v-btn
+                  depressed
+                  class="my-3 mx-7"
+                  elevation="2"
+                  color="green"
+                  x-large
+                >
+                  5</v-btn
+                >
+                <v-btn
+                  depressed
+                  class="my-3 mx-7"
+                  elevation="2"
+                  color="green"
+                  x-large
+                >
+                  10
+                </v-btn>
+                <v-btn
+                  depressed
+                  class="my-3 mx-7"
+                  elevation="2"
+                  color="green"
+                  x-large
+                >
+                  15</v-btn
+                >
+              </div>
 
-					<v-col cols="12" sm="6">
-						<v-card class="ma-5" color="white" height="400" outlined rounded="xl" shaped elevation="2">
-							<v-card-title> Limite de but </v-card-title>
-
-							<div class="d-flex flex-column mb-6">
-								<v-btn depressed class="my-3 mx-7" elevation="2" color="green" x-large>
-									5</v-btn>
-								<v-btn depressed class="my-3 mx-7" elevation="2" color="green" x-large> 10 </v-btn>
-								<v-btn depressed class="my-3 mx-7" elevation="2" color="green" x-large> 15</v-btn>
-							</div>
-
-							<div class="d-flex align-end">
-								<v-btn block elevation="2" fab icon raised rounded color="green"> Jouer
-									<v-icon>mdi-flag-variant
-									</v-icon>
-								</v-btn>
-							</div>
-
-						</v-card>
-					</v-col>
-				</v-row>
-
-
-			</v-card>
-		</v-row>
-
-	</div>
+              <div class="d-flex align-end">
+                <v-btn
+                  block
+                  elevation="2"
+                  fab
+                  icon
+                  raised
+                  rounded
+                  color="green"
+                >
+                  Jouer
+                  <v-icon>mdi-flag-variant </v-icon>
+                </v-btn>
+              </div>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-card>
+    </v-row>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: "Lobby"
-
-	}
-
+export default {
+  name: "Lobby",
+};
 </script>
 
 <style>
-
 </style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,128 +1,171 @@
 <template>
-	<div>
-		<v-row class="ma-5">
-			<v-col cols="6" class="d-flex justify-end">
-				<v-card rounded="xl" class="ma-2 pa-5" height="85vh" max-width="395" elevation="2" outlined>
-					<v-card-text>
-						<v-row>
-							<v-col cols="6">
-								<v-avatar size="120" color="secondary">
-									<img v-if="userInfo.avatar" :src="userInfo.avatar" class="pa-2" />
-									<h1 class="display-1" v-else>{{ $firebase.auth().currentUser.displayName[0].toUpperCase() }}</h1>
-								</v-avatar>
-							</v-col>
-							<v-col cols="6" class="d-flex flex-row-reverse">
-								<h3 style="font-size: 25px">PROFIL</h3>
-							</v-col>
-							<v-col cols="12">
-								<v-form class="d-flex justify-start">
-									<v-container>
+  <div>
+    <v-row class="ma-5">
+      <v-col cols="6" class="d-flex justify-end">
+        <v-card
+          class="ma-2 pa-5"
+          height="85vh"
+          max-width="395"
+          elevation="2"
+          outlined
+        >
+          <v-card-text>
+            <v-row>
+              <v-col cols="6">
+                <v-avatar size="120" color="secondary">
+                  <img
+                    v-if="userInfo.avatar"
+                    :src="userInfo.avatar"
+                    class="pa-2"
+                  />
+                  <h1 class="display-1" v-else>
+                    {{
+                      $firebase.auth().currentUser.displayName[0].toUpperCase()
+                    }}
+                  </h1>
+                </v-avatar>
+              </v-col>
+              <v-col cols="6" class="d-flex flex-row-reverse">
+                <h3 style="font-size: 25px">PROFIL</h3>
+              </v-col>
+              <v-col cols="12">
+                <v-form class="d-flex justify-start">
+                  <v-container>
+                    <v-text-field
+                      v-model="userInfo.username"
+                      label="Nom d'utilisateur"
+                    ></v-text-field>
 
-										<v-text-field v-model="userInfo.username" label="Nom d'utilisateur"></v-text-field>
+                    <v-text-field
+                      v-model="userInfo.email"
+                      label="E-mail"
+                    ></v-text-field>
 
-										<v-text-field v-model="userInfo.email" label="E-mail"></v-text-field>
+                    <v-divider class="my-5"></v-divider>
 
-										<v-divider class="my-5"></v-divider>
+                    <v-text-field
+                      v-model="userInfo.avatar"
+                      label="Avatar"
+                    ></v-text-field>
+                  </v-container>
+                </v-form>
+              </v-col>
+            </v-row>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn color="primary" block @click="update()">Sauvegarder</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
 
-										<v-text-field v-model="userInfo.avatar" label="Avatar"></v-text-field>
+      <v-col cols="6">
+        <v-card
+          class="mb-4 ma-2 pa-5"
+          height="15vh"
+          max-width="395"
+          elevation="2"
+          outlined
+        >
+          <v-card-text>
+            <v-row>
+              <v-col cols="6" offset="6" class="d-flex flex-row-reverse">
+                <h3 style="font-size: 25px">OPTIONS</h3>
+              </v-col>
+              <v-container class="px-0" fluid>
+                <v-row class="">
+                  <v-col class="py-0">
+                    <div class="mt-5" v-if="darkMode == false">
+                      <v-icon class="mr-5"> mdi-white-balance-sunny </v-icon
+                      >Activer le mode jour
+                    </div>
+                    <div class="mt-5" v-if="darkMode == true">
+                      <v-icon class="mr-5"> mdi-weather-night </v-icon>Activer
+                      le mode nuit
+                    </div>
+                  </v-col>
 
-									</v-container>
-								</v-form>
-							</v-col>
-
-						</v-row>
-					</v-card-text>
-					<v-card-actions>
-						<v-btn color="primary" block @click="update()">Sauvegarder</v-btn>
-					</v-card-actions>
-				</v-card>
-			</v-col>
-
-			<v-col cols="6">
-				<v-card rounded="xl" class="mb-4 ma-2 pa-5" height="15vh" max-width="395" elevation="2" outlined>
-					<v-card-text>
-						<v-row>
-							<v-col cols="6" offset="6" class="d-flex flex-row-reverse">
-								<h3 style="font-size: 25px">OPTIONS</h3>
-							</v-col>
-							<v-container class="px-0" fluid>
-
-								<v-row class="">
-									<v-col class="py-0">
-										<div class="mt-5" v-if="darkMode == false">
-											<v-icon class="mr-5">
-												mdi-white-balance-sunny
-											</v-icon>Activer le mode jour
-										</div>
-										<div class="mt-5" v-if="darkMode == true">
-											<v-icon class="mr-5">
-												mdi-weather-night
-											</v-icon>Activer le mode nuit
-										</div>
-									</v-col>
-
-									<v-col cols="auto" class="py-0">
-										<v-switch v-model="$vuetify.theme.dark" @click="darkMode = !darkMode"></v-switch>
-									</v-col>
-								</v-row>
-
-							</v-container>
-						</v-row>
-					</v-card-text>
-				</v-card>
-				<v-card rounded="xl" style="border-radius: 20px" class="d-flex flex-column ma-2 mt-2 pa-5" height="68.4vh" max-width="395" elevation="2" outlined>
-					<v-card-text>
-						<v-row>
-							<v-col cols="6"> </v-col>
-							<v-col cols="6" class="d-flex flex-row-reverse">
-								<h3 style="font-size: 25px">STATS</h3>
-							</v-col>
-						</v-row>
-					</v-card-text>
-				</v-card>
-			</v-col>
-		</v-row>
-	</div>
+                  <v-col cols="auto" class="py-0">
+                    <v-switch
+                      v-model="$vuetify.theme.dark"
+                      @click="darkMode = !darkMode"
+                    ></v-switch>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-row>
+          </v-card-text>
+        </v-card>
+        <v-card
+          class="d-flex flex-column ma-2 mt-2 pa-5"
+          height="68.4vh"
+          max-width="395"
+          elevation="2"
+          outlined
+        >
+          <v-card-text>
+            <v-row>
+              <v-col cols="6"> </v-col>
+              <v-col cols="6" class="d-flex flex-row-reverse">
+                <h3 style="font-size: 25px">STATS</h3>
+              </v-col>
+            </v-row>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: "Profile",
+export default {
+  name: "Profile",
 
-		data() {
-			return {
-				switch1: true,
+  data() {
+    return {
+      switch1: true,
 
-				userInfo: this.$models.user,
-				
-				darkMode: false,
-			}
-		},
-		methods: {
-			update() {
-				this.$firebase.auth().currentUser.updateProfile({
-					displayName: this.userInfo.username,
-					photoURL: this.userInfo.avatar
-				}).then(() => {
-					if(this.userInfo.email !== this.$firebase.auth().currentUser.email) this.$firebase.auth().currentUser.updateEmail(this.userInfo.email);
+      userInfo: this.$models.user,
 
-					this.$db.collection("users").doc(this.$firebase.auth().currentUser.uid).set({
-						username: this.userInfo.username,
-						email: this.userInfo.email,
-						avatar: this.userInfo.avatar,
-					}, {
-						merge: true
-					});
-				});
-			}
-		},
-		created() {
-			this.$db.collection("users").doc(this.$firebase.auth().currentUser.uid).onSnapshot((user) => {
-				this.userInfo = user.data();
-			});
-		}
-	};
+      darkMode: false,
+    };
+  },
+  methods: {
+    update() {
+      this.$firebase
+        .auth()
+        .currentUser.updateProfile({
+          displayName: this.userInfo.username,
+          photoURL: this.userInfo.avatar,
+        })
+        .then(() => {
+          if (this.userInfo.email !== this.$firebase.auth().currentUser.email)
+            this.$firebase.auth().currentUser.updateEmail(this.userInfo.email);
 
+          this.$db
+            .collection("users")
+            .doc(this.$firebase.auth().currentUser.uid)
+            .set(
+              {
+                username: this.userInfo.username,
+                email: this.userInfo.email,
+                avatar: this.userInfo.avatar,
+              },
+              {
+                merge: true,
+              }
+            );
+        });
+    },
+  },
+  created() {
+    this.$db
+      .collection("users")
+      .doc(this.$firebase.auth().currentUser.uid)
+      .onSnapshot((user) => {
+        this.userInfo = user.data();
+      });
+  },
+};
 </script>
 
 <style>
