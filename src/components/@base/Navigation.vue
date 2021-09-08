4a2d6a93408a37653fcafd83cb4bb12665f57a07
Stanislas Castaybert <perso@stan1712.com>
2021-09-08 13:14:56 +0200

    ✨ feature・Ajout d'un retour à l'accueil par le clic sur le nom du projet

--- src/components/@base/Navigation.vue
+++ src/components/@base/Navigation.vue
@@ -2,8 +2,9 @@
 		<v-app-bar app dark v-bind:color="isTransparent" prominent shrink-on-scroll>
 			<v-img alt="Vuetify Logo" class="shrink mr-2" contain src="https://infographart.com/ressources/logos/IFGA/LogoIFGA_1_2x_W.png" transition="scale-transition" width="55" />
 
-			<h1 class="font-weight-thin">{{ $appConfig.name }}</h1>
-		</div>
+			<router-link tag="div" :to="{ path: '/' }" style="cursor: pointer;">
+				<h2 class="font-weight-thin">{{ $appConfig.name }}</h2>
+			</router-link>
 	</v-app-bar>
 </template>
 
