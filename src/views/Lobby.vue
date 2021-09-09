4f89b45484d4405a58656bb2edcf781971345b1f
Stanislas Castaybert <perso@stan1712.com>
2021-09-09 12:35:05 +0200

    🌈 style・Beautify

--- src/views/Lobby.vue
+++ src/views/Lobby.vue
@@ -1,115 +1,61 @@
 <template>
-  <div>
-    <v-container>
-        <v-row>
-          <v-col cols="12" sm="4" offset-sm="2">
-            <v-card
-              class="ma-5"
-              color="white"
-              height="400"
-              elevation="2"
-            >
-              <v-card-title> Limite de temps </v-card-title>
-              <div class="d-flex flex-column mb-6">
-                <v-btn
-                  depressed
-                  class="my-3 mx-7"
-                  color="secondary"
-                  elevation="2"
-                  x-large
-                >
-                  1 min</v-btn
-                >
-                <v-btn
-                  depressed
-                  class="my-3 mx-7"
-                  color="secondary"
-                  elevation="2"
-                  x-large
-                >
-                  2 min</v-btn
-                >
-                <v-btn
-                  depressed
-                  class="my-3 mx-7"
-                  color="secondary"
-                  elevation="2"
-                  x-large
-                >
-                  3 min
-                </v-btn>
-              </div>
+	<div>
+		<v-container>
+			<v-row>
+				<v-col cols="12" sm="4" offset-sm="2">
+					<v-card class="ma-5" color="white" height="400" elevation="2">
+						<v-card-title> Limite de temps </v-card-title>
+						<div class="d-flex flex-column mb-6">
+							<v-btn depressed class="my-3 mx-7" color="secondary" elevation="2" x-large>
+								1 min</v-btn>
+							<v-btn depressed class="my-3 mx-7" color="secondary" elevation="2" x-large>
+								2 min</v-btn>
+							<v-btn depressed class="my-3 mx-7" color="secondary" elevation="2" x-large>
+								3 min
+							</v-btn>
+						</div>
 
-              <div class="mx-7 mt-12">
-                <v-btn block elevation="2" outlined color="primary">
-                  <v-icon class="mr-2">mdi-flag-variant</v-icon>
-                  Jouer
-                </v-btn>
-              </div>
-            </v-card>
-          </v-col>
+						<div class="mx-7 mt-12">
+							<v-btn block elevation="2" outlined color="primary">
+								<v-icon class="mr-2">mdi-flag-variant</v-icon>
+								Jouer
+							</v-btn>
+						</div>
+					</v-card>
+				</v-col>
 
-          <v-col cols="12" sm="4">
-            <v-card
-              class="ma-5"
-              color="white"
-              height="400"
-              elevation="2"
-            >
-              <v-card-title> Limite de but </v-card-title>
+				<v-col cols="12" sm="4">
+					<v-card class="ma-5" color="white" height="400" elevation="2">
+						<v-card-title> Limite de but </v-card-title>
 
-              <div class="d-flex flex-column mb-6">
-                <v-btn
-                  depressed
-                  class="my-3 mx-7"
-                  elevation="2"
-                  color="secondary"
-                  x-large
-                >
-                  5</v-btn
-                >
-                <v-btn
-                  depressed
-                  class="my-3 mx-7"
-                  elevation="2"
-                  color="secondary"
-                  x-large
-                >
-                  10
-                </v-btn>
-                <v-btn
-                  depressed
-                  class="my-3 mx-7"
-                  elevation="2"
-                  color="secondary"
-                  x-large
-                >
-                  15</v-btn
-                >
-              </div>
+						<div class="d-flex flex-column mb-6">
+							<v-btn depressed class="my-3 mx-7" elevation="2" color="secondary" x-large>
+								5</v-btn>
+							<v-btn depressed class="my-3 mx-7" elevation="2" color="secondary" x-large>
+								10
+							</v-btn>
+							<v-btn depressed class="my-3 mx-7" elevation="2" color="secondary" x-large>
+								15</v-btn>
+						</div>
 
-              <div class="mx-7 mt-12">
-                <v-btn
-                  block
-                  elevation="2"
-                  outlined
-                  color="primary"
-                >
-                  <v-icon class="mr-2">mdi-flag-variant</v-icon>
-                  Jouer
-                </v-btn>
-              </div>
-            </v-card>
-          </v-col>
-        </v-row>
-    </v-container>
-  </div>
+						<div class="mx-7 mt-12">
+							<v-btn block elevation="2" outlined color="primary">
+								<v-icon class="mr-2">mdi-flag-variant</v-icon>
+								Jouer
+							</v-btn>
+						</div>
+					</v-card>
+				</v-col>
+			</v-row>
+		</v-container>
+	</div>
 </template>
 
 <script>
-export default {
-  name: "Lobby",
-};
+	export default {
+		name: "Lobby",
+	};
+
 </script>
 
 <style>
