133b5deebf49041d9e9f6ab1690d9df8b625e5cb
Carlier Matheo <Carlier.matheo33@gmail.com>
2021-09-09 10:11:50 +0200

    modification des cards + crédits complet

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,7 +42,7 @@
 			<section id="description">
 				<v-row>
 					<v-col cols="12" sm="6">
-						<v-card rounded="xl" class="mb-4 ma-2 pa-4" height="300" width="700" elevation="2" outlined>
+						<v-card class="mb-4 ma-2 pa-4" height="300" width="700" elevation="2" >
 							<v-card-title class="d-flex justify-center"> Description du jeu </v-card-title>
 
 							<v-card-text class="d-flex justify-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</v-card-text>
@@ -54,40 +54,26 @@
 				</v-row>
 			</section>
 
+			<section class="mb-4 ma-2 pa-4">
+				<v-row >
+					<v-col cols="40" sm="8" >
+						<v-card class="mb-4 ma-2 pa-4" height="350" width="700" elevation="2" >
+							<v-card-title class="d-flex justify-center"> Crédits </v-card-title>
 
-			<v-row>
-				<v-col cols="12" sm="6">
-					<v-img src="https://picsum.photos/700" />
-				</v-col>
-				<v-col cols="12" sm="6">
-					<v-card rounded="xl" class="mb-4 ma-2 pa-4" height="300" width="700" elevation="2" outlined>
-						<v-card-title class="d-flex justify-center"> Trailler </v-card-title>
-					</v-card>
-				</v-col>
-
-
-			</v-row>
-
-
-			<v-row>
-				<v-col cols="12" sm="6">
-					<v-card rounded="xl" class="mb-4 ma-2 pa-4" height="300" width="700" elevation="2" outlined>
-						<v-card-title class="d-flex justify-center"> Crédits </v-card-title>
-
-						<p class="d-flex justify-center">Projet Workshop réalisé sur 5 jours par quatre B2 de l'EPSI Bordeaux et 2 SN2.</p>
-						<v-list class="d-flex justify-center"> <br>
-							Stanislas Castaybert <br>
-							Kilyan Maillard <br>
-							Begüm Cetin <br>
-							Matheo Carlier <br>
-
-						</v-list>
-					</v-card>
-				</v-col>
-				<v-col cols="12" sm="6">
-					<v-img src="https://picsum.photos/600" />
-				</v-col>
-			</v-row>
+							<p class="d-flex justify-center">Projet Workshop réalisé sur 5 jours par quatre B2 de l'EPSI Bordeaux et 2 SN2.</p>
+							<v-list class="d-flex justify-center"> <br>
+								Stanislas Castaybert <br>
+								Kilyan Maillard <br>
+								Begüm Cetin <br>
+								Matheo Carlier <br>
+								Theo Melendreras <br>
+								Ilias Lazaar <br>
+								Gaignierre Cyrille
+							</v-list>
+						</v-card>
+					</v-col>
+				</v-row>
+			</section>
 		</v-container>
 	</div>
 </template>
